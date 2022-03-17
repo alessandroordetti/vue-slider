da13cadec104b04d664ecf917252ebb65ed25579
alessandroordetti <alessandro.ordetti@gmail.com>
2022-03-17 17:12:35 +0100

    Carosello con funzioni

---
+++ main.js
@@ -0,0 +1,52 @@
+const app = new Vue (
+    {
+        el: '#app',
+        data: {
+            activeImage : 0,
+            images: [
+                {
+                    name: 'Daredevil',
+                    image: 'img/daredevil.jpg',
+                    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+                },
+                {
+                    name: 'The Sinner',
+                    image: 'img/sinner.jpg',
+                    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+                },
+                {
+                    name: 'Revolution',
+                    image: 'img/rivoluzione.jpg',
+                    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+                },
+                {
+                    name: 'The Witcher',
+                    image: 'img/witcher.jpg',
+                    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+                },
+                {
+                    name: 'Ghoul',
+                    image: 'img/ghoul.jpg',
+                    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+                },
+            ],
+        },
+
+        methods: {
+            previousSlide () {
+                if (this.activeImage == 0) {
+                    this.activeImage = this.images.length - 1;
+                } else {
+                    this.activeImage--;
+                }
+            },
+            
+            nextSlide () {
+                if (this.activeImage == this.images.length - 1) {
+                    this.activeImage = 0;
+                } else {
+                    this.activeImage++;
+                }
+            }
+        }
+    });
